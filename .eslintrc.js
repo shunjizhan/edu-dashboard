c77630047c55175ede55d042996a66e6945a25db
Shunji Zhan <shunhahaha@live.com>
2021-07-19 21:44:34 +0800

    ini lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,25 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: [2, 2, { SwitchCase: 1 }],
+    quotes: [2, 'single'],
+    semi: [2, 'always'],
+    'linebreak-style': [2, 'unix'],
+    'arrow-parens': [2, 'as-needed'],
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      // 'ForOfStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+
+    /* ---------- turned off ---------- */
+    'max-len': 0,
+    'no-underscore-dangle': 0,
+    'no-multi-spaces': 0,
+    'no-unused-expressions': [2, { allowShortCircuit: true }],              // allow x && y()
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],    // so can import enzyme, which is dev dependencies
   }
 }
